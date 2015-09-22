a8e3a9f7828bcc777d4f69efb59237859663257e
cstdvd <dav.costa90@gmail.com>
2015-09-22 15:25:11 +0200

    Errori risolti per EOF

--- lz77.c
+++ lz77.c
@@ -2,6 +2,7 @@
 
 unsigned char buffer[SB_SIZE];
 unsigned char lookahead[LA_SIZE];
+int la_size;    // actual lookahead size
 
 int main()
 {
@@ -11,6 +12,7 @@ int main()
     unsigned char c;
 	struct token *t = NULL;
     int sb_index = 0, la_index = 0;
+    la_size = LA_SIZE;
 	
 	// open the file
 	file = fopen("./abc.txt", "r");
@@ -18,22 +20,22 @@ int main()
 		printf("Error opening file.\n");
 		exit(1);
 	}
-    
-    for(i = 0; i < SB_SIZE; i++)
-        buffer[i] = ' ';
 	
     for(i = 0; i < LA_SIZE; i++){
         ret = fread(&c, 1, 1, file);
-        if(feof(file))
+        if(feof(file) == 0 && ret != 0)
             lookahead[i] = c;
         else
             break;
     }
     
-    t = match(sb_index, la_index);
-    printf("<%d, %d, %c>\n",t->off, t->len, t->next);
+    for(i = 0; i < LA_SIZE; i++)
+        printf("%c ", lookahead[(la_index+i)%LA_SIZE]);
     
-	while(feof(file) == 0){
+    t = match(sb_index, la_index);
+    printf("\n<%d, %d, %c>\n\n",t->off, t->len, t->next);
+    
+	while(la_size > 0){
 		
         for(i = 0; i < t->len + 1; i++){
             ret = fread(&c, 1, 1, file);
@@ -43,12 +45,20 @@ int main()
                 sb_index = (sb_index + 1) % SB_SIZE;
                 la_index = (la_index + 1) % LA_SIZE;
             }
-            else
-                break;
+            else{ // caso in cui EOF compaia prima del riempimento del lookahead
+                buffer[sb_index] = lookahead[la_index];
+                sb_index = (sb_index + 1) % SB_SIZE;
+                la_index = (la_index + 1) % LA_SIZE;
+                la_size--;
+            }
         }
 	
-		t = match(sb_index, la_index);
-		printf("<%d, %d, %c>\n",t->off, t->len, t->next);
+        for(i = 0; i < la_size; i++)
+            printf("%c ", lookahead[(la_index+i)%LA_SIZE]);
+        
+        if(la_size > 0){
+            t = match(sb_index, la_index);
+            printf("\n<%d, %d, %c>\n\n",t->off, t->len, t->next);}
 	}
 
 	return 0;
@@ -68,15 +78,15 @@ struct token* match(int sb, int la)
 	while(c < SB_SIZE){
 	   
 		for(i = 0; buffer[(sb+i)%SB_SIZE] == lookahead[(la+i)%LA_SIZE]; i++){
-            if((i >= LA_SIZE) || (c+i >= SB_SIZE))
+            if((i >= la_size) || (c+i >= SB_SIZE))
                 break;
 		}
         
         // nel caso in cui il match continui nel lookahead buffer
-        if((c+i >= SB_SIZE) && (i < LA_SIZE)){
+        if((c+i >= SB_SIZE) && (i < la_size)){
             for(j = la; lookahead[j%LA_SIZE] == lookahead[(la+i)%LA_SIZE]; i++){
                 j++;
-                if(i >= LA_SIZE)
+                if(i >= la_size)
                     break;
             }
         }
