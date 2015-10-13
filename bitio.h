fad72957b140fca54052e91f0601a54393abcfe6
Pietro De Rosa <p.derosa0@gmail.com>
2015-10-13 14:05:01 +0200

    Libreria di bit I/O

---
+++ bitio.h
@@ -0,0 +1,37 @@
+/***************************************************************************
+ *          Lempel, Ziv Encoding and Decoding
+ *
+ *   File    : bitio.h
+ *   Authors : David Costa and Pietro De Rosa
+ *
+ ***************************************************************************/
+
+
+
+/***************************************************************************
+ *                                CONSTANTS
+ ***************************************************************************/
+#define BIT_IO_W 0
+#define BIT_IO_R 1
+
+
+
+/***************************************************************************
+ *                            TYPE DEFINITIONS
+ ***************************************************************************/
+struct bitFILE;
+
+
+
+/***************************************************************************
+ *                         FUNCTIONS DECLARATION
+ ***************************************************************************/
+#ifndef bitio_h
+#define bitio_h
+int bitof(int n);
+int bitIO_feof(struct bitFILE *bitF);
+struct bitFILE* bitIO_open(const char *path, int mode);
+int bitIO_close(struct bitFILE *bitF);
+int bitIO_write(struct bitFILE *bitF, void *info, int nbit);
+int bitIO_read(struct bitFILE *bitF, void *info, int info_s, int nbit);
+#endif
